9e97fc289c0051c4b113b8119bf297735e381415
bikash119 <bikashjpatra@gmail.com>
2025-04-06 17:54:17 +0530

    removed console log and unused state

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -3,12 +3,9 @@ import Card from '../components/Card/Card'
 
 export default function Vans() {
     const [vans,setVans] = useState([])
-    console.log(`Rendering : ${vans}`)
-    const [fetchVans,setFetchVans] = useState(false)
     useEffect(() => {(async () => {
         const res = await fetch('/api/vans')
         const data = await res.json()
-        console.log(data.vans)
         setVans(data.vans)
     })();},[])
     
